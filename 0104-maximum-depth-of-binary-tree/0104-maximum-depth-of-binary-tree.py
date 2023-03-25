801e7b69a6c5004adeb5a2c0ed1d2e7871986c92
LeeJaeIk1996 <ly7kdd@naver.com>
2023-03-25 21:39:12 +0900

    Time: 48 ms (32.12%), Space: 15.3 MB (82.10%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -4,27 +4,34 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         # 루트가 비었을 경우 0
         if root is None:
             return 0
         
-        max_depth = 1
-        depth = []   # 깊이를 저장하는 배열
+        queue = deque()
+        queue.append(root)
         
-        def dfs(node: Optional[TreeNode], max_depth):
+        depth = 0
 
-            # 왼쪽 노드가 존재할 경우
-            if node.left:
-               dfs(node.left, max_depth + 1)
+        # 큐가 빌 때까지 반복
+        while queue:
 
-            # 오른쪽 노드가 존재할 경우
-            if node.right:
-                dfs(node.right, max_depth + 1)
+            depth += 1  # 반복된 횟수 = 깊이
 
-            depth.append(max_depth)
+            # 큐에 들어 있는 노드들의 개수만큼 순회
+            for _ in range(len(queue)):
+                cur_node = queue.popleft()
+                # 왼쪽 노드가 존재한다면 큐에 삽입
+                if cur_node.left:
+                    queue.append(cur_node.left)
+                # 오른쪽 노드가 존재한다면 큐에 삽입
+                if cur_node.right:
+                    queue.append(cur_node.right)
 
-        dfs(root, max_depth)
+        # while문이 돌아간 만큼 = 노드 -> 자식노드.. 내려간 만큼
+        return depth
 
-        return max(depth)
