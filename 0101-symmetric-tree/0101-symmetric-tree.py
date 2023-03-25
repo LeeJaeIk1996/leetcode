eb97253d95d818551f24995bf2edd706b5f296da
LeeJaeIk1996 <ly7kdd@naver.com>
2023-03-25 20:27:41 +0900

    Time: 34 ms (73.33%), Space: 13.9 MB (48.96%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -4,31 +4,41 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
 class Solution:
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:       
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         # 루트가 없을 경우 True
         if root is None:
             return True
 
-        def dfs(leftNode: Optional[TreeNode], rightNode:Optional[TreeNode]):
-            # left와 right가 모두 없을 경우 True
+        queue = deque()
+
+        queue.append(root.left)
+        queue.append(root.right)
+
+        while queue:
+            leftNode = queue.popleft()
+            rightNode = queue.popleft()
+
+            # 왼쪽과 오른쪽 모두 비었을 경우 
             if leftNode is None and rightNode is None:
-                return True
+                continue
             
-            # left 혹은 right 둘 중 하나가 없을 경우 False
+            # 왼쪽 혹은 오른쪽 하나만 비었을 경우 
             if leftNode is None or rightNode is None:
                 return False
             
-            # left와 right가 다를 경우 False
+            # 왼쪽 값과 오른쪽 값이 다른 경우
             if leftNode.val != rightNode.val:
                 return False
             
-            # 왼쪽 노드의 왼쪽 값과 오른쪽 값
-            left_right = dfs(leftNode.left, rightNode.right)
+            queue.append(leftNode.left)
+            queue.append(rightNode.right)
+            
+            queue.append(leftNode.right)
+            queue.append(rightNode.left)
 
-            # 오른쪽 노드의 오른쪽 값과 왼쪽 값
-            right_left = dfs(leftNode.right, rightNode.left)
+        # 아무 이상 없을 경우 True를 반환
+        return True
 
-            return left_right and right_left
-        
-        return dfs(root.left, root.right)
