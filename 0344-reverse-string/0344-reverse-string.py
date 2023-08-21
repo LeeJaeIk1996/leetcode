d7a612351443c53667440dad15630b1d08f7abff
LeeJaeIk <ly7kdd@naver.com>
2023-08-21 17:24:05 +0900

    Time: 183 ms (97.21%), Space: 20.8 MB (55.20%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,9 +1,12 @@
-from typing import List
-
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-        s.reverse()
-        
+
+        last = len(s) - 1
+
+        for first in range(len(s)):
+            if last > first:
+                s[first], s[last] = s[last], s[first]
+                last -= 1
